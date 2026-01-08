5702840b120ac1f9bace1fe9905c4dd602e13a1d
Joseph Genio <josephgenio07@gmail.com>
2026-01-08 02:51:02 +0000

    Fix supabase import path

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import supabase from "@/lib/supabase";
+import supabase from "../src/lib/supabase";
 
 export default function Home() {
   const router = useRouter();
